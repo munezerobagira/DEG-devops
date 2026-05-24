bd087ba59d453d07806c2a90f73072632ff62b15
Sostene Munezero Bagira <munezerobagira@gmail.com>
2026-05-24 17:25:01 +0200

    feat: aws resource ec2, rds and s3

--- InfraBlueprint/infra/variables.tf
+++ InfraBlueprint/infra/variables.tf
@@ -30,3 +30,9 @@ variable "s3_bucket_name" {
   description = "Globally unique name for the S3 static assets bucket."
   type        = string
 }
+
+variable "project_tag" {
+  description = "Project tag value applied to all resources"
+  type        = string
+  default     = "vela-payments"
+}
